9da5e7dd9ec7d30d16cd01a45565cdeb0fc5a455
Priyabrath Tripathi <priyavrathtiwariluck@gmail.com>
2024-02-24 17:08:12 +0530

    add user and video model

--- src/utils/asyncHandler.js
+++ src/utils/asyncHandler.js
@@ -1,20 +1,14 @@
-const asyncHandler =(requestHandler)=>{
-    (req,res,next)=>{
-        Promise.resolve(requestHandler(req,res,next)).catch((err)=>next(err))
-    }
-}
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
 
-export {asyncHandler}
+export { asyncHandler };
 
 // const asyncHandler =()=>{}
 // const asyncHandler =(func)=>()=>{}
 // const asyncHandler =(func)=>async()=>{}
-
-
-
-
-
-
 
 // const asyncHandler =(fn)=>async (req,res,next)=>{
 //     try {
